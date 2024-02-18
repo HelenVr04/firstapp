3836fc07b1260ce442b3d2a6a36fdd316995a1e0
HelenVr04 <helenvegaresendiz34@gmail.com>
2024-02-17 19:37:50 -0600

    disenios tailwind 2

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // next.config.js
 module.exports = {
     images: {
-      domains: ['https://img.freepik.com/fotos-premium/fondo-pantalla-rosa-carreras-autos-deportivos_63106-730.jpg'], // Agrega aquí el dominio de tu imagen
+      domains: ['img.freepik.com'], 
     },
   }
   
